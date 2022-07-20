05c16e0b7c8682efdebdf735d5f1924f38ee5dee
unknown <gnebedyuka@list.ru>
2022-07-20 20:42:43 +0500

    добавил папку для файлов клиентов

--- backend/functions/error.go
+++ backend/functions/error.go
@@ -1,9 +1,14 @@
 package functions
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Zefirnutiy/sweet_potato.git/utils"
+)
 
 func HandlerError(err error){
 	if err != nil{
+		utils.Logger.Println(err)
 		fmt.Println(err)
 		return
 	}
