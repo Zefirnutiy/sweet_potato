16c5fa45914c2e4c66938ce5a90c65079aa204f7
The Fir <84587589+Zefirnutiy@users.noreply.github.com>
2022-07-21 21:25:02 +0500

    редактирование sql

--- backend/db/organisation.sql
+++ backend/db/organisation.sql
@@ -6,11 +6,11 @@ CREATE TABLE "Client" (
     LastName varchar(30)  NOT NULL,
     Patronymic varchar(30)  NOT NULL,
     Login varchar(30)  NOT NULL,
-    Password varchar(30)  NOT NULL,
-    Email varchar(256)  NULL,
+    Password varchar(60)  NOT NULL,
+    Email varchar(256)  NOT NULL,
     Telephone varchar(15)  NULL,
     EmailNotifications boolean  default false,
-    GroupId int  NOT NULL,
+    GroupId int  NULL,
     CONSTRAINT Clientpk PRIMARY KEY (Id)
 );
 
@@ -20,7 +20,7 @@ CREATE TABLE "Test" (
     Title varchar(256)  NOT NULL,
     Text text  NOT NULL,
     Date timestamp  NOT NULL,
-    DateDel timestamp  NOT NULL,
+    DateDel timestamp  NULL,
     CONSTRAINT Testpk PRIMARY KEY (Id)
 );
 
@@ -29,8 +29,7 @@ CREATE TABLE "Admin" (
     Id serial  NOT NULL,
     FirstName varchar(20)  NOT NULL,
     LastName varchar(30)  NOT NULL,
-    Login varchar(30)  NOT NULL,
-    Password varchar(30)  NOT NULL,
+    Password varchar(60)  NOT NULL,
     Email varchar(256)  NULL,
     CONSTRAINT Adminpk PRIMARY KEY (Id)
 );
@@ -52,7 +51,7 @@ CREATE TABLE "QuestionResult" (
 	Id serial  NOT NULL,
 	QuestionId int NOT null,
 	ClientId int NOT null,
-	Scores int not null,
+	Scores int NOT null,
 	CONSTRAINT Questionresultpk PRIMARY KEY (Id)
 );
 
@@ -139,8 +138,8 @@ CREATE TABLE "File" (
 --Table: ActiveTest
 CREATE TABLE "ActiveTest" (
     Id serial  NOT NULL,
-    Start timestamp  NOT NULL,
-    End timestamp  NOT NULL,
+    TimeStart timestamp  NOT NULL,
+    TimeEnd timestamp  NOT NULL,
     Time timestamp  NOT NULL,
     Attempts smallint  NOT NULL,
     TestId int  NOT NULL,
