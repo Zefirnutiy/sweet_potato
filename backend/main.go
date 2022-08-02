321ba8bb1038bcc2c85d24c531c24711645f57cb
Zefirnutiy <ssobnaissrgib@dmail.com>
2022-08-03 00:59:58 +0500

    немного изменил авторизацию организации

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	// "github.com/Zefirnutiy/sweet_potato.git/utils"
-	"fmt"
 
 	"github.com/Zefirnutiy/sweet_potato.git/db"
 	"github.com/Zefirnutiy/sweet_potato.git/routes"
@@ -21,18 +20,18 @@ func init() {
 }
 
 func main() {
-	err := db.CreateTable("./db/main.sql", "main")
+	// err := db.CreateTable("./db/main.sql", "main")
 
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	err = db.CreateTable("./db/organisation.sql", "KTK")
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+	// err = db.CreateTable("./db/organisation.sql", "KTK")
 
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
 	
 	// utils.Generate()
 	routes.Routs(cfg.Port)
