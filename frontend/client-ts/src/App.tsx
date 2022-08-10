010437b99853ed486a219fbdb02d3e11f1e9864b
unknown <gnebedyuka@list.ru>
2022-08-10 17:53:31 +0500

    добавил пропсы и чучуть поменял компоненты

--- frontend/client-ts/src/App.tsx
+++ frontend/client-ts/src/App.tsx
@@ -4,19 +4,27 @@ import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
 import { Account } from './pages/Account/Account';
 import { Navbar } from './components/Navbar/Navbar';
+import { InformationCard } from './components/InformationCard/InformationCard';
 
 
 
 const App = () => {
   let links = [
-    {icon: "ff", title: "Something1"},
-    {icon: "cc", title: "Something2"},
+    {icon: "fa fa-cubes", title: "Курсы", path: "#"},
+    {icon: "fa fa-brain", title: "Тесты", path: "#"},
+    {icon: "fa fa-square-poll-horizontal", title: "Результаты", path: "#"},
+    {icon: "fa fa-address-book", title: "Пользователи", path: "#"},
+    {icon: "fa fa-gear", title: "Аккаунт", path: "#"},
+    {icon: "fa fa-circle-info", title: "Информмафия", path: "#"},
+    {icon: "fa fa-chart-column", title: "Статистика", path: "#"},
   ]
 
   if(true){
     return (
     <main>
-      <Navbar/>
+      <Navbar links={links}>
+        <InformationCard title="ТЫ ПИДОР" message="Ты таким родился."/>
+      </Navbar>
       <Routes>
         <Route path="/account" element={<Account />} />
         <Route path="*" element={<Navigate to="/account" />} />
--- frontend/client-ts/src/components/Navbar/Navbar.tsx
+++ frontend/client-ts/src/components/Navbar/Navbar.tsx
@@ -3,29 +3,33 @@ import { Link } from "react-router-dom"
 import { InformationCard } from '../InformationCard/InformationCard';
 import "./Navbar.css"
 import "../../fonts/fontawesome-free-6.1.2-web/css/all.css"
+import { FC } from "react";
+
+type ILink = {
+    icon: string
+    title: string
+    path: string
+}
 
 type PropTypes = {
-    links?: object
+    links: ILink[]
     organizationName?: string
+    children?: React.ReactNode
   };
 
-export const Navbar: React.FC<PropTypes> = ({links, organizationName='Wains'}) =>{
+export const Navbar: FC<PropTypes> = ({links, organizationName='Wains', children}) =>{
     return (
         <div id="navbar">
         <div id="logo">
            {organizationName}
         </div>
         <div id="nav">
-            <Link to={"#"}><i className="fa fa-cubes"></i> Курсы</Link>
-            <Link to={"#"}><i className="fa fa-brain"></i> Тесты</Link>
-            <Link to={"#"}><i className="fa fa-square-poll-horizontal"></i> Результаты</Link>
-            <Link to={"#"}><i className="fa fa-address-book"></i> Пользователи</Link>
-            <Link to={"#"}><i className="fa fa-gear"></i> Аккаунт</Link>
-            <Link to={"#"}><i className="fa fa-circle-info"></i> Информмафия</Link>
-            <Link to={"#"}><i className="fa fa-chart-column"></i> Статистика</Link>
+            {links?.map(link => 
+                <Link to={link.path}><i className={link.icon}></i> {link.title}</Link> 
+            )}
         </div>
         <div id="navContent">
-            <InformationCard title="ТЫ ПИДОР" message="Ты таким родился."/>
+            {children}
         </div>
     </div>
     )
