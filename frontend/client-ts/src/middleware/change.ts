33d4f263395c1d889e4a60c8d7ece7a335a76e9a
unknown <gnebedyuka@list.ru>
2022-08-29 02:47:21 +0500

    поработал над цв. схемами

--- frontend/client-ts/src/middleware/change.ts
+++ frontend/client-ts/src/middleware/change.ts
@@ -1,38 +1,140 @@
 export function changeSchema(){
     document.addEventListener('keydown', function(event) {
       if (event.code === 'KeyZ') {
-        document.documentElement.style.setProperty("--main-hue", "250" )
-        document.documentElement.style.setProperty("--accent-hue", "40" )
+        document.documentElement.style.setProperty("--main-color-1", "" )
+        document.documentElement.style.setProperty("--main-color-2", "" )
+        document.documentElement.style.setProperty("--main-color-3", "" )
+        document.documentElement.style.setProperty("--main-color-4", "" )
+
+        document.documentElement.style.setProperty("--main-dark-color-1", "" )
+        document.documentElement.style.setProperty("--main-dark-color-2", "" )
+        document.documentElement.style.setProperty("--main-dark-color-3", "" )
+        document.documentElement.style.setProperty("--main-dark-color-4", "" )
+
+        document.documentElement.style.setProperty("--accent-color-1", "" )
+        document.documentElement.style.setProperty("--accent-color-2", "" )
+        document.documentElement.style.setProperty("--accent-color-3", "" )
+        document.documentElement.style.setProperty("--accent-color-4", "" )
+
+        document.documentElement.style.setProperty("--accent-dark-color-1", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-2", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-3", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-4", "" )
         return
       } 
   
       if (event.code === 'KeyX') {
-        document.documentElement.style.setProperty("--main-hue", "280" )
-        document.documentElement.style.setProperty("--accent-hue", "40" )
+        document.documentElement.style.setProperty("--main-color-1", "#333333" )
+        document.documentElement.style.setProperty("--main-color-2", "#262626" )
+        document.documentElement.style.setProperty("--main-color-3", "#1a1a1a" )
+        document.documentElement.style.setProperty("--main-color-4", "#0d0d0d" )
+
+        document.documentElement.style.setProperty("--main-dark-color-1", "#0d0d0d" )
+        document.documentElement.style.setProperty("--main-dark-color-2", "#000000" )
+        document.documentElement.style.setProperty("--main-dark-color-3", "#000000" )
+        document.documentElement.style.setProperty("--main-dark-color-4", "#000000" )
+
+        document.documentElement.style.setProperty("--accent-color-1", "#A46FAC" )
+        document.documentElement.style.setProperty("--accent-color-2", "#9a5fa3" )
+        document.documentElement.style.setProperty("--accent-color-3", "#8b5593" )
+        document.documentElement.style.setProperty("--accent-color-4", "#7c4c83" )
+
+        document.documentElement.style.setProperty("--accent-dark-color-1", "#866fac" )
+        document.documentElement.style.setProperty("--accent-dark-color-2", "#785fa3" )
+        document.documentElement.style.setProperty("--accent-dark-color-3", "#6c5593" )
+        document.documentElement.style.setProperty("--accent-dark-color-4", "#604c83" )
         return
       } 
   
       if (event.code === 'KeyC') {
-        document.documentElement.style.setProperty("--main-hue", "310" )
-        document.documentElement.style.setProperty("--accent-hue", "40" )
+        document.documentElement.style.setProperty("--main-color-1", "#8F2EC4" )
+        document.documentElement.style.setProperty("--main-color-2", "#8029af" )
+        document.documentElement.style.setProperty("--main-color-3", "#71249b" )
+        document.documentElement.style.setProperty("--main-color-4", "#621f86" )
+
+        document.documentElement.style.setProperty("--main-dark-color-1", "#570F7F" )
+        document.documentElement.style.setProperty("--main-dark-color-2", "#470c68" )
+        document.documentElement.style.setProperty("--main-dark-color-3", "#380a51" )
+        document.documentElement.style.setProperty("--main-dark-color-4", "#28073b" )
+
+        document.documentElement.style.setProperty("--accent-color-1", "" )
+        document.documentElement.style.setProperty("--accent-color-2", "" )
+        document.documentElement.style.setProperty("--accent-color-3", "" )
+        document.documentElement.style.setProperty("--accent-color-4", "" )
+
+        document.documentElement.style.setProperty("--accent-dark-color-1", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-2", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-3", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-4", "" )
         return
       } 
   
       if (event.code === 'KeyV') {
-        document.documentElement.style.setProperty("--main-hue", "340" )
-        document.documentElement.style.setProperty("--accent-hue", "40" )
+        document.documentElement.style.setProperty("--main-color-1", "#FF8E31" )
+        document.documentElement.style.setProperty("--main-color-2", "#ff8018" )
+        document.documentElement.style.setProperty("--main-color-3", "#fd7200" )
+        document.documentElement.style.setProperty("--main-color-4", "#e46700" )
+
+        document.documentElement.style.setProperty("--main-dark-color-1", "#A65410" )
+        document.documentElement.style.setProperty("--main-dark-color-2", "#8f480e" )
+        document.documentElement.style.setProperty("--main-dark-color-3", "#773c0c" )
+        document.documentElement.style.setProperty("--main-dark-color-4", "#603109" )
+
+        document.documentElement.style.setProperty("--accent-color-1", "" )
+        document.documentElement.style.setProperty("--accent-color-2", "" )
+        document.documentElement.style.setProperty("--accent-color-3", "" )
+        document.documentElement.style.setProperty("--accent-color-4", "" )
+
+        document.documentElement.style.setProperty("--accent-dark-color-1", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-2", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-3", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-4", "" )
         return
       }
 
       if (event.code === 'KeyB') {
-        document.documentElement.style.setProperty("--main-hue", "370" )
-        document.documentElement.style.setProperty("--accent-hue", "40" )
+        document.documentElement.style.setProperty("--main-color-1", "" )
+        document.documentElement.style.setProperty("--main-color-2", "" )
+        document.documentElement.style.setProperty("--main-color-3", "" )
+        document.documentElement.style.setProperty("--main-color-4", "" )
+
+        document.documentElement.style.setProperty("--main-dark-color-1", "" )
+        document.documentElement.style.setProperty("--main-dark-color-2", "" )
+        document.documentElement.style.setProperty("--main-dark-color-3", "" )
+        document.documentElement.style.setProperty("--main-dark-color-4", "" )
+
+        document.documentElement.style.setProperty("--accent-color-1", "" )
+        document.documentElement.style.setProperty("--accent-color-2", "" )
+        document.documentElement.style.setProperty("--accent-color-3", "" )
+        document.documentElement.style.setProperty("--accent-color-4", "" )
+
+        document.documentElement.style.setProperty("--accent-dark-color-1", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-2", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-3", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-4", "" )
         return
       }
 
       if (event.code === 'KeyN') {
-        document.documentElement.style.setProperty("--main-hue", "0" )
-        document.documentElement.style.setProperty("--accent-hue", "40" )
+        document.documentElement.style.setProperty("--main-color-1", "" )
+        document.documentElement.style.setProperty("--main-color-2", "" )
+        document.documentElement.style.setProperty("--main-color-3", "" )
+        document.documentElement.style.setProperty("--main-color-4", "" )
+
+        document.documentElement.style.setProperty("--main-dark-color-1", "" )
+        document.documentElement.style.setProperty("--main-dark-color-2", "" )
+        document.documentElement.style.setProperty("--main-dark-color-3", "" )
+        document.documentElement.style.setProperty("--main-dark-color-4", "" )
+
+        document.documentElement.style.setProperty("--accent-color-1", "" )
+        document.documentElement.style.setProperty("--accent-color-2", "" )
+        document.documentElement.style.setProperty("--accent-color-3", "" )
+        document.documentElement.style.setProperty("--accent-color-4", "" )
+
+        document.documentElement.style.setProperty("--accent-dark-color-1", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-2", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-3", "" )
+        document.documentElement.style.setProperty("--accent-dark-color-4", "" )
         return
       }
     });
