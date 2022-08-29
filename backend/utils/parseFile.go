0fb5db1b5710f4eb0d7c294a8b5904007621a625
Zefirnutiy <ssobnaissrgib@dmail.com>
2022-08-29 10:16:23 +0500

    парсер готов на 30%

--- backend/utils/parseFile.go
+++ backend/utils/parseFile.go
@@ -7,38 +7,93 @@ import (
 	"strings"
 )
 
-type OneChoise struct{
-	Question string
-	Answers []string
-	CorrectAnswers []string
+var globalId = 0
+var globalStr = "%^$"
+type InputButton struct{
+	Id 				int
+	Question 		string
+	Answers 		[]string
+	CorrectAnswers  []string
+	Hint 			string
+	Type 			string
+}
+
+type InputText struct {
+	Id			int
+	Question	string
+	Answer		string
+}
+
+type InpuntConstraint struct{
+	Id 			int
+	Question	string
+	Answers		map[string]string
 }
 
 func SortQuestion(path string){
 	allQuestion := strings.Split(parseFile(path), "\n\n")
 
-
-	for _, value := range allQuestion {
-		getAnswers(value, "#")
-		break
+	for _, value := range allQuestion{
+		typeAns := getType(value)
+		switch typeAns{
+		
+		case "*":
+			fmt.Println(oneChoise(value, typeAns))
+		case "#":
+			fmt.Println(oneChoise(value, typeAns))
+		}
+		
 	}
 }
-
-func getAnswers(str string, simbol string) {
-	answers := ""
-	correctAnswer := ""
+func oneChoise(str, simbol string) InputButton {
+	trueAns := ""
+	otherAns := ""
+	hint := ""
 	strMas := strings.Split(str, "\n")
-	for index, val := range strMas {
-		if index != 0{
-
+	for index, val := range strMas{
+		if index != 0 {
 			if strings.Contains(val, simbol){
-				correctAnswer += strings.ReplaceAll(val, simbol, "") + "^&"
-				}else{
-					answers += strings.ReplaceAll(val, simbol, "")
-				}
+				val = strings.ReplaceAll(val, simbol, "")
+				trueAns += val 
+			}else if strings.Contains(val, "~"){
+				val = strings.ReplaceAll(val, "~", "")
+				hint = val
+			}else{
+				otherAns += val + globalStr
 			}
+		}
 	}
-	
+	globalId += 1
+	return InputButton{globalId, strMas[0], strings.Split(otherAns, globalStr), strings.Split(trueAns, globalStr), hint, simbol}
 }
+
+
+func getType(str string) string{
+	
+	if strings.Contains(str, "*") {
+		return "*"
+	}
+
+	if strings.Contains(str, "#") {
+		return "#"
+	}
+
+	if strings.Contains(str, "[") && strings.Contains(str, "]") {
+		return "[]"
+	}
+
+	if strings.Contains(str, "=") {
+		return "="
+	}
+
+	if strings.Contains(str, "1. ") {
+		return "1. "
+	}
+
+	return "empty"
+}
+
+
 
 func parseFile(path string) string{
 	file, err := os.Open(path)
